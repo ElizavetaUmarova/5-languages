e94344f89c3edf8f2ed0a9a6857852ab402a0ce3
Elizaveta <lisabeth.umarova@yandex.com>
2025-09-14 13:14:55 +0300

    сделала

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const userLanguage = prompt('Какой у тебя язык ?');
 
 
 switch (true) {
-    case userLanguage === 'EN':
+    case userLanguage === 'en':
       console.log('Good afternoon!');
       break;
-    case userLanguage === 'RU':
+    case userLanguage === 'ru':
        console.log('Добрый день!');
     break;
-    case userLanguage === 'DE':
-       console.log('Guten Tag!');
+    case userLanguage === 'de':
+       console.log('Gutten tag!');
     break;
-    case userLanguage === 'El':
+    case userLanguage === 'el':
        console.log('Καλησπέρα!');
     break;
-    case userLanguage === 'PT':
+    case userLanguage === 'pt':
        console.log('Boa tarde!');
     break;
     default:
