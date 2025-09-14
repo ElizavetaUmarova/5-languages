5049fbaa0a554a19931578afe53c68c31e195806
Elizaveta <lisabeth.umarova@yandex.com>
2025-09-14 13:06:24 +0300

    Мой первый коммит

---
+++ script.js
@@ -0,0 +1,19 @@
+const userLanguage = prompt('Какой у тебя язык ?');
+
+
+switch (true) {
+    case userLanguage === 'EN':
+      console.log('Good afternoon!');
+      break;
+    case userLanguage === 'RU':
+       console.log('Добрый день!');
+    break;
+    case userLanguage === 'DE':
+       console.log('Guten Tag!');
+    break;
+    case userLanguage === 'El':
+       console.log('Καλησπέρα!');
+    break;
+    default:
+    console.log('Ошибка');
+}
